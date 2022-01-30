87d01f8a3efbcaa7e76f51e4f39cee5d92ccea7c
PeaBrane <yrpei@ucsd.edu>
2022-01-30 13:43:48 -0800

    implemented wolff

--- lattices.py
+++ lattices.py
@@ -13,9 +13,9 @@ class Ising():
         self.coupling_dims = lattice_shape + (self.n_dims,)
 
         self.n_replicas = len(temp_list)
+        self.temp_list = temp_list
         self.temp_ids = np.arange(self.n_replicas)
         self.replica_ids = np.arange(self.n_replicas)
-        self.temp_list = temp_list
 
         if couplings == 'ferro':
             self.couplings = np.ones(self.coupling_dims)
@@ -24,16 +24,19 @@ class Ising():
         elif couplings == 'gaussian':
             self.couplings = randn(*self.coupling_dims)
 
-        couplings = [coupling for coupling in self.couplings.transpose([self.n_dims] + list(range(self.n_dims)))]
+        couplings = [coupling for coupling in np.moveaxis(self.couplings, -1, 0)]
         couplings_clone = [np.roll(self.couplings[..., i], 1, i) for i in range(self.n_dims)]
         self.couplings_doubled = np.stack(couplings + couplings_clone, axis=-1)
 
         self.reset()
 
     def reset(self):
+        self.temp_ids = np.arange(self.n_replicas)
+        self.replica_ids = np.arange(self.n_replicas)
+
         self.n_sweeps = 0
         self.spins = -1 + 2 * rand(self.n_replicas, *self.lattice_shape).round()
-        self.energies = utils.get_energy(self.spins, self.couplings)
+        self.energies, self.interactions = utils.get_energy(self.spins, self.couplings)
  
         self.mags_aggregate = 0
         self.energies_aggregate = 0
@@ -44,16 +47,18 @@ class Ising():
         self.energies_average = np.zeros(self.n_replicas)
 
     def update(self):
+        self.n_sweeps += 1
+        self.energies, self.interactions = utils.get_energy(self.spins[self.temp_ids], self.couplings)
+        
+        self.spins_aggregate += self.spins
+        self.mags_aggregate += self.spins[self.temp_ids].mean(tuple(range(1, self.n_dims+1)))
+        self.energies_aggregate += self.energies
+        self.correlations_aggregate += self.spins[tuple([0]*self.n_dims)] * self.spins
+
         self.mags_average = self.mags_aggregate / self.n_sweeps
         self.energies_average = self.energies_aggregate / self.n_sweeps
 
-    # def __repr__(self):
-    #     mag, energy = self.mag_aggregate / self.n_sweeps, self.energy_aggregate / self.n_sweeps
-
-    #     rep = f'shape: {self.lattice_shape}, magnetization: {mag}, energy: {energy}'
-    #     return rep
-
-    def sweep(self, n_sweeps=1): 
+    def sweep(self, n_sweeps=1, cluster_update=False): 
         rand_block = np.log(rand(n_sweeps, self.n_replicas, *(self.lattice_shape))) \
             * np.expand_dims(self.temp_list[self.replica_ids], tuple(range(1, self.n_dims+1))) / 2
 
@@ -66,15 +71,25 @@ class Ising():
                     utils.get_local_field(spins_transposed, self.couplings_doubled, self.n_replicas, self.n_dims, self.lattice_shape, coordinate)
                 update_mask = -spins_transposed[coordinate] * local_field >= rand_list[coordinate]
                 spins_transposed[coordinate][update_mask] = -spins_transposed[coordinate][update_mask]
-
-            self.n_sweeps += 1
+            
             self.spins = np.moveaxis(spins_transposed, -1, 0)
-            self.energies = utils.get_energy(self.spins[self.temp_ids], self.couplings)
+            self.update()
 
-            self.spins_aggregate += self.spins
-            self.mags_aggregate += self.spins[self.temp_ids].mean(tuple(range(1, self.n_dims+1)))
-            self.energies_aggregate += self.energies
-            self.correlations_aggregate += self.spins[tuple([0]*self.n_dims)] * self.spins
+            if cluster_update:
+                self.cluster_update()
+
+    def cluster_update(self):
+        spins = self.spins.reshape([self.n_replicas, -1])
+
+        for (replica_id, temp, interaction) in zip(self.replica_ids, self.temp_list, self.interactions):
+            interaction = (1 - np.exp(-2 * interaction / temp)) >= rand(self.n_dims, *self.lattice_shape)
+            clusters = utils.get_clusters(interaction)
+            cluster_id = clusters[np.random.choice(clusters.size)]
+            
+            spins[replica_id][clusters == cluster_id] = -spins[replica_id][clusters == cluster_id]
+            self.spins = spins.reshape(self.n_replicas, *self.lattice_shape)
+
+        self.update()
 
     # def simulated_annealing(self, T_high, T_low, n_sweeps, space='linear'):
     #     beta_low, beta_high = 1 / T_high, 1 / T_low
@@ -88,9 +103,9 @@ class Ising():
     #     for temp in temp_list:
     #         self.sweep(temp)
 
-    def parallel_tempering(self, n_sweeps, exchange_interval=1):
+    def parallel_tempering(self, n_sweeps, cluster_update=False, exchange_interval=1):
         for sweep_id in range(n_sweeps):
-            self.sweep()
+            self.sweep(cluster_update=cluster_update)
             
             if sweep_id % exchange_interval == 0:
                 temp_id = np.random.choice(self.n_replicas - 1)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from math import prod
 import numpy as np
+from pyparsing import col
+from scipy.sparse import csr_matrix, csgraph
 
 
 def get_local_field(spins_transposed, couplings_doubled, n_replicas, n_dims, lattice_shape, coordinate):
@@ -16,7 +18,20 @@ def get_energy(spins, couplings):
     n_dims = len(spins.shape) - 1
 
     spins_rolled = np.stack([np.roll(spins, -1, i) for i in range(1, n_dims+1)], axis=1)  # [n_replicas, n_dims, (shape)]
-    energies = spins[:, np.newaxis, ...] * spins_rolled * np.moveaxis(couplings, -1, 0)  # [n_replicas, n_dims, (shape)]
-    energies = energies.sum(tuple(range(1, n_dims+2))) / spins[0].size
+    interactions = spins[:, np.newaxis, ...] * spins_rolled * np.moveaxis(couplings, -1, 0)  # [n_replicas, n_dims, (shape)]
+    energies = interactions.sum(tuple(range(1, n_dims+2))) / spins[0].size
 
-    return energies
+    return energies, interactions
+
+
+def get_clusters(interaction):
+    n_spins = interaction[0].size
+    spin_ids = np.arange(n_spins).reshape(interaction[0].shape)
+
+    rows = [interact.flatten().nonzero()[0] for interact in interaction]
+    columns = [np.roll(spin_ids, -1, dim).take(row) for (dim, row) in enumerate(rows)]
+    rows, columns = np.hstack(rows), np.hstack(columns)
+
+    G_sparse = csr_matrix((np.ones_like(rows, dtype='bool'), (rows, columns)), shape=(n_spins, n_spins))
+    clusters = csgraph.connected_components(G_sparse)[1]
+    return clusters
