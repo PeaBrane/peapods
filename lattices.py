1868ae532d54adb0c591d12e2db44118a488a611
PeaBrane <yrpei@ucsd.edu>
2022-01-30 11:11:29 -0800

    added parallel tempering

--- lattices.py
+++ lattices.py
@@ -1,67 +1,104 @@
 import time
 import numpy as np
-from numpy.random import rand, randn, choice
+from numpy.random import rand, randn
 import itertools
 
 import utils
 
 
 class Ising():
-    def __init__(self, dims, couplings='ferro'):
-        self.n_dims = len(dims)
-        self.dims = dims
-        self.coupling_dims = dims + (self.n_dims,)
+    def __init__(self, lattice_shape, couplings='ferro', temp_list=np.geomspace(0.1, 10, 20)):
+        self.n_dims = len(lattice_shape)
+        self.lattice_shape = tuple(lattice_shape)
+        self.coupling_dims = lattice_shape + (self.n_dims,)
+
+        self.n_replicas = len(temp_list)
+        self.temp_ids = np.arange(self.n_replicas)
+        self.replica_ids = np.arange(self.n_replicas)
+        self.temp_list = temp_list
 
         if couplings == 'ferro':
             self.couplings = np.ones(self.coupling_dims)
         elif couplings == 'bimodal':
-            self.couplings = rand(*self.coupling_dims).around()
-        elif couplings == 'gauss':
+            self.couplings = -1 + 2*rand(*self.coupling_dims).round()
+        elif couplings == 'gaussian':
             self.couplings = randn(*self.coupling_dims)
+
+        couplings = [coupling for coupling in self.couplings.transpose([self.n_dims] + list(range(self.n_dims)))]
+        couplings_clone = [np.roll(self.couplings[..., i], 1, i) for i in range(self.n_dims)]
+        self.couplings_doubled = np.stack(couplings + couplings_clone, axis=-1)
 
         self.reset()
 
     def reset(self):
-        self.spins = -1 + 2 * rand(*self.dims)
-        self.magnetizations = np.zeros(self.dims)
-        self.correlations = np.zeros(self.dims)
-        self.correlations_truncated = np.zeros(self.dims)
-        self.energy = 0
+        self.n_sweeps = 0
+        self.spins = -1 + 2 * rand(self.n_replicas, *self.lattice_shape).round()
+        self.energies = utils.get_energy(self.spins, self.couplings)
+ 
+        self.mags_aggregate = 0
+        self.energies_aggregate = 0
+        self.spins_aggregate = np.zeros_like(self.spins)
+        self.correlations_aggregate = np.zeros_like(self.spins)
 
-    def sweep(self, beta, n_sweeps=1):
-        for _ in range(n_sweeps):
-            rand_list = -np.log(rand(*self.dims)) / self.spins / beta / 2
+        self.mags_average = np.zeros(self.n_replicas)
+        self.energies_average = np.zeros(self.n_replicas)
 
-            for coordinate in itertools.product(*[list(range(dim)) for dim in self.dims]):
-                local_field = utils.get_local_field(self.spins, self.couplings, self.dims, coordinate)
-                if rand_list[coordinate] >= local_field:
-                    self.spins[coordinate] = -self.spins[coordinate]
+    def update(self):
+        self.mags_average = self.mags_aggregate / self.n_sweeps
+        self.energies_average = self.energies_aggregate / self.n_sweeps
 
-            self.magnetizations += self.spins
-            self.correlations += self.spins[tuple([0]*self.n_dims)] * self.spins
-            self.energy += utils.get_energy(self.spins, self.couplings)
+    # def __repr__(self):
+    #     mag, energy = self.mag_aggregate / self.n_sweeps, self.energy_aggregate / self.n_sweeps
 
-        self.magnetizations /= n_sweeps
-        self.correlations /= n_sweeps
-        self.energy /= n_sweeps
-        self.correlations_truncated = self.correlations - self.magnetizations**2
+    #     rep = f'shape: {self.lattice_shape}, magnetization: {mag}, energy: {energy}'
+    #     return rep
 
-    def simulated_annealing(self, temperature_high, temperature_low, n_sweeps, space='linear'):
-        beta_low, beta_high = 1 / temperature_high, 1 / temperature_low
-        if space == 'linear':
-            beta_list = np.linspace(beta_low, beta_high, n_sweeps)
-        elif space == 'log':
-            beta_list = np.geomspace(beta_low, beta_high, n_sweeps)
-        else:
-            raise ValueError("space argument has to be either 'linear' or 'log'")
+    def sweep(self, n_sweeps=1): 
+        rand_block = np.log(rand(n_sweeps, self.n_replicas, *(self.lattice_shape))) \
+            * np.expand_dims(self.temp_list[self.replica_ids], tuple(range(1, self.n_dims+1))) / 2
 
-        for sweep in range(n_sweeps):
-            self.sweep(beta_list[sweep])
+        for sweep_id in range(n_sweeps):
+            spins_transposed = np.moveaxis(self.spins, 0, -1)
+            rand_list = np.moveaxis(rand_block[sweep_id], 0, -1)
 
+            for coordinate in itertools.product(*[list(range(dim)) for dim in self.lattice_shape]):
+                local_field = \
+                    utils.get_local_field(spins_transposed, self.couplings_doubled, self.n_replicas, self.n_dims, self.lattice_shape, coordinate)
+                update_mask = -spins_transposed[coordinate] * local_field >= rand_list[coordinate]
+                spins_transposed[coordinate][update_mask] = -spins_transposed[coordinate][update_mask]
 
-model = Ising((40, 40))
-start = time.time()
-model.simulated_annealing(10, 0.1, 2**6)
-end = time.time()
-print(end - start)
-print(model.energy)
+            self.n_sweeps += 1
+            self.spins = np.moveaxis(spins_transposed, -1, 0)
+            self.energies = utils.get_energy(self.spins[self.temp_ids], self.couplings)
+
+            self.spins_aggregate += self.spins
+            self.mags_aggregate += self.spins[self.temp_ids].mean(tuple(range(1, self.n_dims+1)))
+            self.energies_aggregate += self.energies
+            self.correlations_aggregate += self.spins[tuple([0]*self.n_dims)] * self.spins
+
+    # def simulated_annealing(self, T_high, T_low, n_sweeps, space='linear'):
+    #     beta_low, beta_high = 1 / T_high, 1 / T_low
+    #     if space == 'linear':
+    #         temp_list = 1 / np.linspace(beta_low, beta_high, n_sweeps)
+    #     elif space == 'log':
+    #         temp_list = 1 / np.geomspace(beta_low, beta_high, n_sweeps)
+    #     else:
+    #         raise ValueError("space argument has to be either 'linear' or 'log'")
+
+    #     for temp in temp_list:
+    #         self.sweep(temp)
+
+    def parallel_tempering(self, n_sweeps, exchange_interval=1):
+        for sweep_id in range(n_sweeps):
+            self.sweep()
+            
+            if sweep_id % exchange_interval == 0:
+                temp_id = np.random.choice(self.n_replicas - 1)
+                temp_1, temp_2 = self.temp_list[temp_id], self.temp_list[temp_id + 1]
+                index_1, index_2 = (np.argwhere(self.temp_ids == temp_idx)[0, 0] for temp_idx in [temp_id, temp_id+1])
+                energy_1, energy_2 = (self.energies[temp_idx] for temp_idx in [temp_id, temp_id+1])
+                
+                if (energy_2 - energy_1) * (1 / temp_1 - 1 / temp_2) >= np.log(rand()):
+                    self.temp_ids[index_1], self.temp_ids[index_2] = self.temp_ids[index_2], self.temp_ids[index_1]
+                    self.replica_ids = (self.temp_ids == np.arange(self.n_replicas)[..., np.newaxis]).argmax(1)
+                    
--- utils.py
+++ utils.py
@@ -1,26 +1,22 @@
+from math import prod
 import numpy as np
 
 
-def get_local_field(spins, couplings, dims, coordinate):
-    n_dims = len(dims)
-    local_field = 0
+def get_local_field(spins_transposed, couplings_doubled, n_replicas, n_dims, lattice_shape, coordinate):
+    neighbors = np.array(coordinate)[..., np.newaxis] + np.hstack([np.eye(n_dims), -np.eye(n_dims)]).astype('int64')
+    neighbors = np.mod(neighbors, np.array(lattice_shape)[..., np.newaxis])
+    neighbors = np.ravel_multi_index(neighbors, lattice_shape)
+    neighbors = (neighbors * n_replicas) + np.arange(n_replicas)[..., np.newaxis]  # [n_replicas, neighbors]
 
-    for i in range(n_dims):
-        neighbor = list(coordinate)
-        neighbor[i] = (coordinate[i] + 1) % dims[i]
-        neighbor = tuple(neighbor)
-        local_field += spins[neighbor] * couplings[coordinate + (i,)]
-    for i in range(n_dims):
-        neighbor = list(coordinate)
-        neighbor[i] = (coordinate[i] - 1) % dims[i]
-        neighbor = tuple(neighbor)
-        local_field += spins[neighbor] * couplings[neighbor + (i,)]
-
+    local_field = (spins_transposed.take(neighbors) * couplings_doubled[coordinate]).sum(1)
     return local_field
 
 
 def get_energy(spins, couplings):
-    n_dims = spins.ndim
-    energy = sum([np.sum(spins * np.roll(spins, -1, i) * couplings[..., i])
-                  for i in range(n_dims)])
-    return energy
+    n_dims = len(spins.shape) - 1
+
+    spins_rolled = np.stack([np.roll(spins, -1, i) for i in range(1, n_dims+1)], axis=1)  # [n_replicas, n_dims, (shape)]
+    energies = spins[:, np.newaxis, ...] * spins_rolled * np.moveaxis(couplings, -1, 0)  # [n_replicas, n_dims, (shape)]
+    energies = energies.sum(tuple(range(1, n_dims+2))) / spins[0].size
+
+    return energies
