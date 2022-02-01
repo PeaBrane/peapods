70dbb8ad618931def8906402bbdfc0b58101a58d
PeaBrane <yrpei@ucsd.edu>
2022-01-31 16:10:33 -0800

    added numba implementation of single spin-flips

--- spin_models.py
+++ spin_models.py
@@ -28,6 +28,8 @@ class Ising():
         couplings_clone = [np.roll(self.couplings[..., i], 1, i) for i in range(self.n_dims)]
         self.couplings_doubled = np.stack(couplings + couplings_clone, axis=-1)
 
+        self.neighbors = utils.get_neighbors(lattice_shape)
+
         self.reset()
 
     def reset(self):
@@ -46,35 +48,35 @@ class Ising():
         self.mags_average = np.zeros(self.n_replicas)
         self.energies_average = np.zeros(self.n_replicas)
 
+        self.mags_aggregate_2 = 0
+        self.mags_aggregate_4 = 0
+
     def update(self):
         self.n_sweeps += 1
+        self.mags = self.spins[self.temp_ids].mean(tuple(range(1, self.n_dims+1)))
         self.energies, self.interactions = utils.get_energy(self.spins[self.temp_ids], self.couplings)
         
         self.spins_aggregate += self.spins
-        self.mags_aggregate += self.spins[self.temp_ids].mean(tuple(range(1, self.n_dims+1)))
+        self.mags_aggregate += self.mags
         self.energies_aggregate += self.energies
-        self.correlations_aggregate += self.spins[tuple([0]*self.n_dims)] * self.spins
+        self.correlations_aggregate += self.spins * \
+            np.expand_dims(np.moveaxis(self.spins, 0, -1)[tuple([0]*self.n_dims)], tuple(range(1, self.n_dims+1)))
 
+        self.spins_average = self.spins_aggregate / self.n_sweeps
         self.mags_average = self.mags_aggregate / self.n_sweeps
         self.energies_average = self.energies_aggregate / self.n_sweeps
+        self.correlations_average = self.correlations_aggregate / self.n_sweeps - \
+            self.spins_average * np.expand_dims(np.moveaxis(self.spins_average, 0, -1)[tuple([0]*self.n_dims)], tuple(range(1, self.n_dims+1)))
+        
+        self.mags_aggregate_2 += self.mags**2
+        self.mags_aggregate_4 += self.mags**4
+        if self.n_sweeps != 0 and self.n_sweeps % 10 == 0:
+            self.binder_cumulant = 1 - (self.mags_aggregate_4 / self.n_sweeps) / (3 * (self.mags_aggregate_2 / self.n_sweeps)**2)
 
-    def sweep(self, n_sweeps=1, cluster_update=False): 
-        rand_block = np.log(rand(n_sweeps, self.n_replicas, *(self.lattice_shape))) \
-            * np.expand_dims(self.temp_list[self.replica_ids], tuple(range(1, self.n_dims+1))) / 2
-
-        for sweep_id in range(n_sweeps):
-            spins_transposed = np.moveaxis(self.spins, 0, -1)
-            rand_list = np.moveaxis(rand_block[sweep_id], 0, -1)
-
-            for coordinate in itertools.product(*[list(range(dim)) for dim in self.lattice_shape]):
-                local_field = \
-                    utils.get_local_field(spins_transposed, self.couplings_doubled, self.n_replicas, self.n_dims, self.lattice_shape, coordinate)
-                update_mask = -spins_transposed[coordinate] * local_field >= rand_list[coordinate]
-                spins_transposed[coordinate][update_mask] = -spins_transposed[coordinate][update_mask]
-            
-            self.spins = np.moveaxis(spins_transposed, -1, 0)
+    def update_spins(self, n_sweeps=1, cluster_update=False): 
+        for _ in range(n_sweeps):
+            self.spins = utils.sweep(self.spins, self.couplings_doubled, self.neighbors, self.temp_list[self.replica_ids])
             self.update()
-
             if cluster_update:
                 self.cluster_update()
 
@@ -105,7 +107,7 @@ class Ising():
 
     def parallel_tempering(self, n_sweeps, cluster_update=False, exchange_interval=1):
         for sweep_id in range(n_sweeps):
-            self.sweep(cluster_update=cluster_update)
+            self.update_spins(cluster_update=cluster_update)
             
             if sweep_id % exchange_interval == 0:
                 temp_id = np.random.choice(self.n_replicas - 1)
--- utils.py
+++ utils.py
@@ -1,17 +1,58 @@
 from math import prod
 import numpy as np
-from pyparsing import col
 from scipy.sparse import csr_matrix, csgraph
+import numba
 
 
-def get_local_field(spins_transposed, couplings_doubled, n_replicas, n_dims, lattice_shape, coordinate):
-    neighbors = np.array(coordinate)[..., np.newaxis] + np.hstack([np.eye(n_dims), -np.eye(n_dims)]).astype('int64')
-    neighbors = np.mod(neighbors, np.array(lattice_shape)[..., np.newaxis])
-    neighbors = np.ravel_multi_index(neighbors, lattice_shape)
-    neighbors = (neighbors * n_replicas) + np.arange(n_replicas)[..., np.newaxis]  # [n_replicas, neighbors]
+def get_neighbors(lattice_shape):
+    inds = np.arange(prod(lattice_shape))
+    inds = list(np.unravel_index(inds, lattice_shape))
+    neighbors = []
 
-    local_field = (spins_transposed.take(neighbors) * couplings_doubled[coordinate]).sum(1)
-    return local_field
+    for dim, dim_size in enumerate(lattice_shape):
+        inds_new = inds.copy()
+        inds_new[dim] = (inds_new[dim] + 1) % dim_size
+        neighbor = np.ravel_multi_index(inds_new, lattice_shape)
+        neighbors.append(neighbor)
+
+    for dim, dim_size in enumerate(lattice_shape):
+        inds_new = inds.copy()
+        inds_new[dim] = (inds_new[dim] - 1) % dim_size
+        neighbor = np.ravel_multi_index(inds_new, lattice_shape)
+        neighbors.append(neighbor)
+
+    return np.vstack(neighbors).T
+
+
+@numba.njit()
+def sweep(spins, couplings_doubled, neighbors, temp_list):
+    n_replicas = spins.shape[0]
+    lattice_shape = spins.shape[1:]
+
+    spins = spins.reshape((n_replicas, -1))  # [n_replicas, num_spins]
+    num_spins = spins.shape[1]
+    couplings_doubled = couplings_doubled.reshape((-1, couplings_doubled.shape[-1]))  # [num_spins, n_coupling]
+    rand_block = (np.log(np.random.rand(num_spins, n_replicas)) * temp_list).T / 2  # [n_replicas, num_spins]
+
+    for replica_id in range(n_replicas):
+        spin = spins[replica_id]
+        rand_list = rand_block[replica_id]
+        for spin_id in range(num_spins):
+            if -(spin[spin_id] * spin.take(neighbors[spin_id]) * couplings_doubled[spin_id]).sum() >= rand_list[spin_id]:
+                spin[spin_id] = -spin[spin_id]
+        spins[replica_id] = spin
+
+    return spins.reshape(n_replicas, *lattice_shape)
+
+
+# def get_local_field(spins_transposed, couplings_doubled, n_replicas, n_dims, lattice_shape, coordinate):
+#     neighbors = np.array(coordinate)[..., np.newaxis] + np.hstack([np.eye(n_dims), -np.eye(n_dims)]).astype('int64')
+#     neighbors = np.mod(neighbors, np.array(lattice_shape)[..., np.newaxis])
+#     neighbors = np.ravel_multi_index(neighbors, lattice_shape)
+#     neighbors = (neighbors * n_replicas) + np.arange(n_replicas)[..., np.newaxis]  # [n_replicas, neighbors]
+
+#     local_field = (spins_transposed.take(neighbors) * couplings_doubled[coordinate]).sum(1)
+#     return local_field
 
 
 def get_energy(spins, couplings):
