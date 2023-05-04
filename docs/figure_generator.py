55b56677a903b6a67ef3b80bf68d4f6ffd6b5501
PeaBrane <yrpei@ucsd.edu>
2023-05-03 17:30:58 -0700

    cleaner figure

--- docs/figure_generator.py
+++ docs/figure_generator.py
@@ -29,7 +29,7 @@ temperatures = np.geomspace(0.1, 10, 32)
 ising = IsingEnsemble(lattice_shape=(32, 32),
                       n_ensemble=32,
                       temperatures=temperatures)
-ising.sample(n_sweeps=2**22,
+ising.sample(n_sweeps=2**24,
              warmup_ratio=0.1,
              cluster_update_interval=2**3,
              pt_interval=2**3)
@@ -41,6 +41,7 @@ csds[csds == 0] = np.nan
 plt.plot(np.arange(1, 32**2+1), csds)
 plt.xscale('log')
 plt.yscale('log')
+plt.ylim(1e-7, 1)
 plt.legend([f"$T = {temp:.3f}$" for temp in temperatures[17:27:2]])
 plt.title("cluster size distributions of a 32 x 32 Ising ferromagnet")
 plt.savefig("csd.png", dpi=300, bbox_inches='tight', pad_inches=0, transparent=True)
