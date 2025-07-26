fc1e7b6b8d4379ca5fd2df130aff92d4cecc374f
PeaBrane <yanrpei@gmail.com>
2025-07-26 11:22:45 -0700

    do not transpose in sweep (big perf boost)

--- sweeps.py
+++ sweeps.py
@@ -27,41 +27,59 @@ def sweep(
     """
     n_replicas = spins.shape[0]
     lattice_shape = spins.shape[1:]
+    num_spins = np.prod(lattice_shape)
 
-    spins = spins.reshape((n_replicas, -1)).T  # [num_spins, n_replicas]
-    num_spins = spins.shape[0]
-    couplings_doubled = couplings_doubled.reshape(
-        (-1, couplings_doubled.shape[-1])
-    )  # [num_spins, n_coupling]
+    # Create views without transpose - shape: (n_replicas, num_spins)
+    spins_flat = spins.reshape((n_replicas, num_spins))
+    couplings_flat = couplings_doubled.reshape((num_spins, -1))
+    neighbors_flat = neighbors.reshape((num_spins, -1))
 
     match mode:
         case "metropolis":
-            rand_block = np.log(np.random.rand(num_spins, n_replicas)) * temperatures
+            # Shape: (n_replicas, num_spins) - better memory layout
+            rand_block = (
+                np.log(np.random.rand(n_replicas, num_spins))
+                * temperatures[:, np.newaxis]
+            )
             rand_block = (rand_block / 2).astype(np.float32)
         case "gibbs":
-            rand_block = logit(np.random.rand(num_spins, n_replicas)) * temperatures
+            rand_block = (
+                logit(np.random.rand(n_replicas, num_spins))
+                * temperatures[:, np.newaxis]
+            )
             rand_block = (rand_block / 2).astype(np.float32)
         case _:
             raise ValueError("Invalid sampling mode.")
 
-    spins = sweep_numba(spins, couplings_doubled, neighbors, rand_block)
-    # return spins.reshape(n_replicas, *lattice_shape)
-    return spins.T.reshape(n_replicas, *lattice_shape)
-
-
-@numba.njit("f4[:, :](f4[:, :], f4[:, :], i4[:, :], f4[:, :])")
-def sweep_numba(spins, couplings_doubled, neighbors, rand_block):
-    num_spins = spins.shape[0]
-    for spin_id in range(num_spins):
-        spin = spins[spin_id]
-
-        local_fields = (
-            spins[neighbors[spin_id]] * np.expand_dims(couplings_doubled[spin_id], 1)
-        ).sum(0)
-        eng_changes = -spin * local_fields
-        flip_mask = eng_changes >= rand_block[spin_id]
-        spin[flip_mask] = -spin[flip_mask]
-
-        spins[spin_id] = spin
-
+    sweep_numba(spins_flat, couplings_flat, neighbors_flat, rand_block)
     return spins
+
+
+@numba.njit("void(f4[:, :], f4[:, :], i4[:, :], f4[:, :])", fastmath=True)
+def sweep_numba(spins, couplings_doubled, neighbors, rand_block):
+    """
+    Modified to work with shape (n_replicas, num_spins) instead of (num_spins, n_replicas)
+    This avoids transpose operations and improves cache locality.
+    """
+    n_replicas, num_spins = spins.shape
+
+    for spin_id in range(num_spins):
+        # Get neighbors for this spin
+        neighbor_indices = neighbors[spin_id]
+        couplings = couplings_doubled[spin_id]
+
+        # Process all replicas for this spin
+        for replica_id in range(n_replicas):
+            spin = spins[replica_id, spin_id]
+
+            # Calculate local field
+            local_field = 0.0
+            for i in range(len(neighbor_indices)):
+                local_field += spins[replica_id, neighbor_indices[i]] * couplings[i]
+
+            # Energy change if we flip this spin
+            eng_change = -spin * local_field
+
+            # Metropolis criterion
+            if eng_change >= rand_block[replica_id, spin_id]:
+                spins[replica_id, spin_id] = -spin
